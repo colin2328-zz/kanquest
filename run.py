60cf39d3838db975fa87f8b13029689b46f73003
hanksahin <hanksahin@gmail.com>
2015-04-01 15:28:30 -0500

    Test parameters

--- run.py
+++ run.py
@@ -4,9 +4,22 @@ from player import Player
 me = Player('Colin', Human, 200)
 hakan = Player('Hakan', Elf, 300)
 
+
+print ('Starting states:')
 me.print_state()
+hakan.print_state()
+
+print ('Hakan attacks Colin:')
 hakan.attack(me)
 me.print_state()
+hakan.print_state()
 
+print ('Colin buys units:')
 me.buy_units(10)
 me.print_state()
+hakan.print_state()
+
+print ('Colin attacks Hakan')
+me.attack(hakan)
+me.print_state()
+hakan.print_state()
